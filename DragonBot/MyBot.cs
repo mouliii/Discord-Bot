b28139ef60800777ccaa239bd5ff51caa0fb71d8
Allu <Allu@ALLU-PC>
2017-05-11 12:34:37 +0300

    added files

---
+++ DragonBot/MyBot.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace DragonBot
+{
+    class MyBot
+    {
+        DiscordClient discord;
+
+        public MyBot()
+        {
+            discord = new DiscordClient(x =>
+            {
+                x.LogLevel = LogSeverity.Info;
+                x.LogHandler = Log;
+            });
+
+            discord.UsingCommands(x =>
+            {
+                x.PrefixChar = '!';
+                x.AllowMentionPrefix = true;
+            });
+            // COMMANDS
+            var commands = discord.GetService<CommandService>();
+
+            commands.CreateCommand("Hello")
+                .Do(async (e) =>
+                {
+                    await e.Channel.SendMessage("╭∩╮( ͡° ͜ʖ ͡°)");
+                });
+            // EXECUTE
+            discord.ExecuteAndWait(async () =>
+            {
+                await discord.Connect("MzEyMTQ4NzUwMTQ1NDIxMzEz.C_W5sw.DTrZKzykj_VPk0Br_28AZ-jV6GM", TokenType.Bot);
+            });
+        }
+
+        private void Log(object sender, LogMessageEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
